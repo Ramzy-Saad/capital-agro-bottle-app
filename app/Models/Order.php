c94e9200dc14d40453ad9f04f529f82e5cef797f
Ramzy Saad <www.ramzysaad16@gmail.com>
2026-04-08 14:02:31 +0200

    main models

---
+++ app/Models/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $guarded = [];
+    public function bottle()
+    {
+        return $this->belongsTo(Bottle::class);
+    }
+
+    public function ingredients()
+    {
+        return $this->belongsToMany(Ingredient::class)
+            ->withPivot('price', 'quantity');
+    }
+}
